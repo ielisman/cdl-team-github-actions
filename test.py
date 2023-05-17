13e00b6c4d729b4d025bbef723da239875670382
ielisman <73816588+ielisman@users.noreply.github.com>
2023-05-17 19:05:30 -0400

    Update test.py

--- test.py
+++ test.py
@@ -27,14 +27,14 @@ title = driver.title
 print (f"Opening {os.getenv('GITHUB_ENV')} environment variables file and writing new variables to that")
 env_file = os.getenv('GITHUB_ENV') # Get the path of the runner file
 with open(env_file, "a") as repo_env_file:
-  repo_env_file.write(f"TITLE='{title}'")
-  repo_env_file.write(f"TEST123='TESTOK123'")
-  #repo_env_file.write(f"RUNRESULT='title={title}'")
-  print(f"wrote TITLE='{title}'")
-  print(f"wrote TEST123='TESTOK123'")
-  #print(f"wrote RUNRESULT='title={title}'")
- 
-# Print the website title to the console
+  repo_env_file.write(f"TITLE='{title}'\n")
+  repo_env_file.write(f"TEST123='TESTOK123'\n")
+  repo_env_file.write(f"RUNRESULT='title={title}'\n")
+  print(f"wrote TITLE='{title}'\n")
+  print(f"wrote TEST123='TESTOK123'\n")
+  print(f"wrote RUNRESULT='title={title}'\n")
+
+  # Print the website title to the console
 print(f"title={title}; URL={os.environ['URL']}; LOGIN={os.environ['LOGIN']}; ")
 
 # Close the webdriver
