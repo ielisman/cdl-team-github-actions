b8397689059f685cf026ae0041243fe65bb98efe
ielisman <73816588+ielisman@users.noreply.github.com>
2023-05-19 20:08:01 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,16 @@
 import base64
 import firebase_admin
 import os
-import sys
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 from firebase_admin import credentials, firestore
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
-url = ""
-if (len(sys.argv) > 2):
-  url = sys.argv[2]
-else:
-  print("URL is not supplied. Quiting")
-  quit()
   
 private_key_str = os.environ['CDL_TEAM_KEY']
 private_key = RSA.import_key(private_key_str)
-encrypted_message = 'JHrSklI0NQ7ZwqVHrr7AMvU3X9R3znWOyT4+4hkpDsHGq00+QycRYT1E2tTw0OqefsJd5bdTvjj5YXyRKeb01g1J6LDy7QXIo1H+wSqfO1OwZv3RsaF/V/mx19JLoAtjYkqW6Fm1xAo/PgTuXbAUaOj8eUptSVU1pbT4Wxle8eAW6/LZAZtDBTbgnGPpfeUhpMIPPI/JOAsVk60z+T5QatApW54Keo1+l/qfmgg1eEhZVkLkPqsLiCeYoEPEzRe6XwMdv7EmADcJBvqv6iXwPRMvFiCmu4wPiWw/CxXf2XC5pAuyfRGVAgeQh4u17DjaExbUFDJP3UbUTYX3k82iUw=='
+encrypted_message = os.environ['ACTION']
 cipher_rsa = PKCS1_OAEP.new(private_key)
 message_bytes = base64.b64decode(encrypted_message)
 decrypted_message = cipher_rsa.decrypt(message_bytes).decode()  
@@ -43,7 +35,7 @@ chrome_options.add_argument('--headless')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
 # Navigate to the website URL
-driver.get(url)
+driver.get("https://github.com")
 
 # Retrieve the website title
 title = driver.title
