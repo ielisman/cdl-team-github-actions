3228015292802fb0cd4ae8db3c3dde9fdece23c3
ielisman <73816588+ielisman@users.noreply.github.com>
2023-05-19 20:37:42 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import base64
 import firebase_admin
+import json
 import os
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
@@ -13,8 +14,11 @@ private_key = RSA.import_key(private_key_str)
 encrypted_message = os.environ['ACTION']
 cipher_rsa = PKCS1_OAEP.new(private_key)
 message_bytes = base64.b64decode(encrypted_message)
-decrypted_message = cipher_rsa.decrypt(message_bytes).decode()  
-  
+decrypted_message = cipher_rsa.decrypt(message_bytes).decode()
+json_string = decrypted_message.replace("'", '"')
+json_object = json.loads(json_string)
+output = f"Retrieved {json_object['firstName']} {json_object['lastName']} with coursesId {json_object['coursesId']}"
+
 firebase_credentials = os.environ['FIREBASE_JSON']
 firebase_creds_file = 'firebase-creds.json'
 with open(firebase_creds_file, 'w') as file:
@@ -24,7 +28,7 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 doc_ref = db.collection('messages').document()
 doc_ref.set({
-    'message': decrypted_message
+    'message': output
 })  
 
 # Configure Chrome options for headless mode
