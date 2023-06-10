487e964ecc3323b2e2c8c47c42a3084b5f814aae
ielisman <ielisman@gmail.com>
2023-06-09 20:15:08 -0400

    general try except to catch all errors

--- enroll.py
+++ enroll.py
@@ -159,151 +159,153 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 if not args.enroll:
     print("Dry run: will not enroll student")
 
-# LOGIN
-driver.get(const.KELLER_URL)
-driver.find_element(by=By.ID, value=const.LOGIN_PAGE_ID_EMAIL).clear()
-driver.find_element(by=By.ID, value=const.LOGIN_PAGE_ID_EMAIL).send_keys(const.LOGIN_PAGE_VAL_EMAIL)
-driver.find_element(by=By.ID, value=const.LOGIN_PAGE_ID_PASSWORD).clear()
-driver.find_element(by=By.ID, value=const.LOGIN_PAGE_ID_PASSWORD).send_keys(const.LOGIN_PAGE_VAL_PASSWORD)  
-driver.find_element(by=By.ID, value=const.LOGIN_PAGE_ID_SIGNIN).click()
-
-# DASHBOARD - verify if logged in
 try:
-    signout = driver.find_element(by=By.ID, value=const.DASH_PAGE_ID_SIGNOUT)
-    if (signout.text != const.DASH_PAGE_VAL_SIGNOUT):
-        writeToFirestore('Not Enrolled',f"Login error: Do not see sign out in Dashboard {fln}", isQuit=True) 
-except:
-    writeToFirestore('Not Enrolled',f"Login error: cannot login {fln}", isQuit=True)
+    # LOGIN
+    driver.get(const.KELLER_URL)
+    driver.find_element(by=By.ID, value=const.LOGIN_PAGE_ID_EMAIL).clear()
+    driver.find_element(by=By.ID, value=const.LOGIN_PAGE_ID_EMAIL).send_keys(const.LOGIN_PAGE_VAL_EMAIL)
+    driver.find_element(by=By.ID, value=const.LOGIN_PAGE_ID_PASSWORD).clear()
+    driver.find_element(by=By.ID, value=const.LOGIN_PAGE_ID_PASSWORD).send_keys(const.LOGIN_PAGE_VAL_PASSWORD)  
+    driver.find_element(by=By.ID, value=const.LOGIN_PAGE_ID_SIGNIN).click()
 
-# STUDENTS PAGE
-aMyStudents = driver.find_element(by=By.LINK_TEXT, value=const.DASH_PAGE_ATXT_MYSTUDENTS)
-aMyStudents.click()
-try:
-    searchBtn = driver.find_element(by=By.ID, value=const.STUDENT_PAGE_ID_SEARCH)
-    searchBtnType = searchBtn.get_attribute(const.STUDENT_PAGE_ATT_TP_SEARCH)
-    if searchBtnType != const.STUDENT_PAGE_ATT_VAL_SEARCH:
-        writeToFirestore('Not Enrolled',f"error: Dont see {const.STUDENT_PAGE_ATT_TP_SEARCH}={const.STUDENT_PAGE_ATT_VAL_SEARCH} in {const.STUDENT_PAGE_ID_SEARCH}. Student {fln}", isQuit=True)
-except:
-    writeToFirestore('Not Enrolled',f"error: cannot find student page elements for {fln}", isQuit=True)
-
-# SEARCH FOR SPECIFIC STUDENT  
-txtId = driver.find_element(by=By.ID, value=const.STUDENT_PAGE_ID_ID)
-txtId.clear()
-txtId.send_keys(id)
-try:
-    grid = driver.find_element(by=By.ID, value=const.STUDENT_PAGE_ID_GRID)
-    searchBtn = driver.find_element(by=By.ID, value=const.STUDENT_PAGE_ID_SEARCH)            
-    searchBtn.click()            
-    WebDriverWait(driver, 20).until(EC.staleness_of(grid))
-except:
-    writeToFirestore('Not Enrolled',f"Search: unable to submit user search for {fln}", isQuit=True)
-    
-# SEE IF ANY RECORDS RETURNED PER STUDENT
-isStudentRegistered = False
-try: # only parsing first 50 records
-    totalStudentsFound = driver.find_element(by=By.ID, value=const.STUDENT_PAGE_ID_STUD_COUNT)
-    count = int(totalStudentsFound.text)
-    if count != 0:
-        isStudentRegistered = True
-        print (f"Found id {id}")
-except:
-    writeToFirestore('Not Enrolled',f"Search Record: unable to retrieve user search for {fln}", isQuit=True)
-
-# STUDENT IS NOT FOUND, REGISTER THAT STUDENT
-if not isStudentRegistered:
-    print(f"Registering student with id {id}")
+    # DASHBOARD - verify if logged in
     try:
-        result = addStudent(firstName=firstName, lastName=lastName, email=email, phone=phone, id=id, location=const.DEFAULT_LOCATION)
-        if not result:
-             writeToFirestore('Not Enrolled',f"Save Record: Unable to save student {fln}", isQuit=True)
-        else:
-            driver.get(const.DASH_URL)
+        signout = driver.find_element(by=By.ID, value=const.DASH_PAGE_ID_SIGNOUT)
+        if (signout.text != const.DASH_PAGE_VAL_SIGNOUT):
+            writeToFirestore('Not Enrolled',f"Login error: Do not see sign out in Dashboard {fln}", isQuit=True) 
     except:
-        writeToFirestore('Not Enrolled',f"Save Record: exception while save student {fln}", isQuit=True)
-else:
-    print (f"{id} is already registered in the system")
+        writeToFirestore('Not Enrolled',f"Login error: cannot login {fln}", isQuit=True)
 
-WebDriverWait(driver, 20).until_not (EC.invisibility_of_element ( (By.ID, const.DASH_PAGE_ID_ONLINETRAINING) ))
+    # STUDENTS PAGE
+    aMyStudents = driver.find_element(by=By.LINK_TEXT, value=const.DASH_PAGE_ATXT_MYSTUDENTS)
+    aMyStudents.click()
+    try:
+        searchBtn = driver.find_element(by=By.ID, value=const.STUDENT_PAGE_ID_SEARCH)
+        searchBtnType = searchBtn.get_attribute(const.STUDENT_PAGE_ATT_TP_SEARCH)
+        if searchBtnType != const.STUDENT_PAGE_ATT_VAL_SEARCH:
+            writeToFirestore('Not Enrolled',f"error: Dont see {const.STUDENT_PAGE_ATT_TP_SEARCH}={const.STUDENT_PAGE_ATT_VAL_SEARCH} in {const.STUDENT_PAGE_ID_SEARCH}. Student {fln}", isQuit=True)
+    except:
+        writeToFirestore('Not Enrolled',f"error: cannot find student page elements for {fln}", isQuit=True)
 
-# OPEN COURSES PAGE, SELECT A COURSE, SEARCH, FIND AND SELECT STUDENT TO ENROLL
-driver.get(f"{const.ENROLL_PAGE_CURRICULUM_URL_PRE}{coursesId}")
-if is_element_present(by=By.ID, value=const.ENROLL_PAGE_ID_COURSE_NAME):
-    courseName = driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_COURSE_NAME).text
-    print(f"Course name for {coursesId} is {courseName}")
-driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_ENROLL_STUDENTS).click()
-driver.get(f"{const.ENROLL_PAGE_STENRBATCH_URL_PRE}{coursesId}")
-WebDriverWait(driver, 20).until_not (EC.invisibility_of_element ( (By.ID, const.ENROLL_PAGE_ID_PAGER_SEARCH) ))
-dataPage = driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_PAGER_SEARCH)
-driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_LAST_NAME).clear()
-driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_LAST_NAME).send_keys(lastName)
-driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_SEARCH).click()
-WebDriverWait(driver, 20).until(EC.staleness_of(dataPage))
-trList = driver.find_elements(by=By.XPATH, value=const.ENROLL_PAGE_TBL_SEARCH_RES)
-i=0
-flagEnroll = False
-for tr in trList:
-    if i>0:
-        if (tr.text != const.ENROLL_PAGE_TBL_NOT_FOUND):
-            recIndex = i-1
-            recId = tr.find_element(by=By.ID, value=f"{const.ENROLL_PAGE_TBL_STUDID_PRE}{recIndex}")
-            if recId.text == id:
-                flagEnroll = True
-                selectRec = tr.find_element(by=By.ID, value=f"{const.ENROLL_PAGE_TBL_STUDSEL_PRE}{recIndex}")
-                selectRec.click()
-    i = i + 1
-if not flagEnroll:
-    writeToFirestore('Not Enrolled',f"Cannot select record for {fln}", isQuit=True)
+    # SEARCH FOR SPECIFIC STUDENT  
+    txtId = driver.find_element(by=By.ID, value=const.STUDENT_PAGE_ID_ID)
+    txtId.clear()
+    txtId.send_keys(id)
+    try:
+        grid = driver.find_element(by=By.ID, value=const.STUDENT_PAGE_ID_GRID)
+        searchBtn = driver.find_element(by=By.ID, value=const.STUDENT_PAGE_ID_SEARCH)            
+        searchBtn.click()            
+        WebDriverWait(driver, 20).until(EC.staleness_of(grid))
+    except:
+        writeToFirestore('Not Enrolled',f"Search: unable to submit user search for {fln}", isQuit=True)
+        
+    # SEE IF ANY RECORDS RETURNED PER STUDENT
+    isStudentRegistered = False
+    try: # only parsing first 50 records
+        totalStudentsFound = driver.find_element(by=By.ID, value=const.STUDENT_PAGE_ID_STUD_COUNT)
+        count = int(totalStudentsFound.text)
+        if count != 0:
+            isStudentRegistered = True
+            print (f"Found id {id}")
+    except:
+        writeToFirestore('Not Enrolled',f"Search Record: unable to retrieve user search for {fln}", isQuit=True)
 
-# SET 80 SCORE
-driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_CONTINUE).click()
-WebDriverWait(driver, 20).until_not (EC.invisibility_of_element ( (By.ID, const.ENROLL_PAGE_ID_CHECKALL) ))
-driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_CHECKALL).click()
-driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_CONTINUE2).click()
-WebDriverWait(driver, 20).until_not (EC.invisibility_of_element ( (By.ID, const.ENROLL_PAGE_ID_SEL_SCORE) ))
-Select(driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_SEL_SCORE)).select_by_visible_text(const.ENROLL_PAGE_VAL_SEL_SCORE)
-driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_APPLY).click()
-driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_CONTINUE3).click()
+    # STUDENT IS NOT FOUND, REGISTER THAT STUDENT
+    if not isStudentRegistered:
+        print(f"Registering student with id {id}")
+        try:
+            result = addStudent(firstName=firstName, lastName=lastName, email=email, phone=phone, id=id, location=const.DEFAULT_LOCATION)
+            if not result:
+                writeToFirestore('Not Enrolled',f"Save Record: Unable to save student {fln}", isQuit=True)
+            else:
+                driver.get(const.DASH_URL)
+        except:
+            writeToFirestore('Not Enrolled',f"Save Record: exception while save student {fln}", isQuit=True)
+    else:
+        print (f"{id} is already registered in the system")
 
-driver.implicitly_wait(10)
+    WebDriverWait(driver, 20).until_not (EC.invisibility_of_element ( (By.ID, const.DASH_PAGE_ID_ONLINETRAINING) ))
 
-# SET DUE DATES, CONFIRM SELECTION AND ENROLL STUDENT
-expirationDate = date.today() + timedelta(days=const.ENROLL_PAGE_EXPIRATION_DAYS)
-remindDate = expirationDate -  timedelta(days=const.ENROLL_PAGE_REMIND_BEFORE_EXP)
-strExpirationDate = expirationDate.strftime(const.ENROLL_PAGE_DATE_FORMAT)
-strRemindDate = remindDate.strftime(const.ENROLL_PAGE_DATE_FORMAT)
-driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_REMINDER).clear()
-driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_REMINDER).send_keys(strRemindDate)
-driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_EXPIRATION).clear()
-driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_EXPIRATION).send_keys(strExpirationDate)
-driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_CONTINUE4).click()
-driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_CONTINUE5).click()
-if (args.enroll):
-  driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_COMPLETE_ENROLL).click()
+    # OPEN COURSES PAGE, SELECT A COURSE, SEARCH, FIND AND SELECT STUDENT TO ENROLL
+    driver.get(f"{const.ENROLL_PAGE_CURRICULUM_URL_PRE}{coursesId}")
+    if is_element_present(by=By.ID, value=const.ENROLL_PAGE_ID_COURSE_NAME):
+        courseName = driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_COURSE_NAME).text
+        print(f"Course name for {coursesId} is {courseName}")
+    driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_ENROLL_STUDENTS).click()
+    driver.get(f"{const.ENROLL_PAGE_STENRBATCH_URL_PRE}{coursesId}")
+    WebDriverWait(driver, 20).until_not (EC.invisibility_of_element ( (By.ID, const.ENROLL_PAGE_ID_PAGER_SEARCH) ))
+    dataPage = driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_PAGER_SEARCH)
+    driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_LAST_NAME).clear()
+    driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_LAST_NAME).send_keys(lastName)
+    driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_SEARCH).click()
+    WebDriverWait(driver, 20).until(EC.staleness_of(dataPage))
+    trList = driver.find_elements(by=By.XPATH, value=const.ENROLL_PAGE_TBL_SEARCH_RES)
+    i=0
+    flagEnroll = False
+    for tr in trList:
+        if i>0:
+            if (tr.text != const.ENROLL_PAGE_TBL_NOT_FOUND):
+                recIndex = i-1
+                recId = tr.find_element(by=By.ID, value=f"{const.ENROLL_PAGE_TBL_STUDID_PRE}{recIndex}")
+                if recId.text == id:
+                    flagEnroll = True
+                    selectRec = tr.find_element(by=By.ID, value=f"{const.ENROLL_PAGE_TBL_STUDSEL_PRE}{recIndex}")
+                    selectRec.click()
+        i = i + 1
+    if not flagEnroll:
+        writeToFirestore('Not Enrolled',f"Cannot select record for {fln}", isQuit=True)
 
-# VERIFY THAT STUDENT IS ACTUALLY ENROLLED
-driver.get(const.VERIFY_PAGE_STUDENTS_PAGE)
-txtId = driver.find_element(by=By.ID, value=const.STUDENT_PAGE_ID_ID)
-txtId.clear()
-txtId.send_keys(id)
-try:
-    grid = driver.find_element(by=By.ID, value=const.STUDENT_PAGE_ID_GRID)
-    searchBtn = driver.find_element(by=By.ID, value=const.STUDENT_PAGE_ID_SEARCH)            
-    searchBtn.click()            
-    WebDriverWait(driver, 20).until(EC.staleness_of(grid))    
-except:
-    writeToFirestore('Not Enrolled',f"Search: unable to submit user search for {fln}", courseName=courseName, isQuit=True)
+    # SET 80 SCORE
+    driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_CONTINUE).click()
+    WebDriverWait(driver, 20).until_not (EC.invisibility_of_element ( (By.ID, const.ENROLL_PAGE_ID_CHECKALL) ))
+    driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_CHECKALL).click()
+    driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_CONTINUE2).click()
+    WebDriverWait(driver, 20).until_not (EC.invisibility_of_element ( (By.ID, const.ENROLL_PAGE_ID_SEL_SCORE) ))
+    Select(driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_SEL_SCORE)).select_by_visible_text(const.ENROLL_PAGE_VAL_SEL_SCORE)
+    driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_APPLY).click()
+    driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_CONTINUE3).click()
 
-if not is_element_present(by=By.ID, value=const.VERIFY_PAGE_ID_RECORD_ONE):
-    writeToFirestore('Not Enrolled',f"Verify enrollement: cannot find student with id {id} and name {fln}", courseName=courseName, isQuit=True)
+    driver.implicitly_wait(10)
 
-# for each course, there must be identification. below is only for ELDT Theory
-driver.find_element(by=By.ID, value=const.VERIFY_PAGE_ID_RECORD_ONE).click()
-#if not is_element_present(by=By.ID, value=const.VERIFY_PAGE_ID_REC_37):
-if not is_element_present(by=By.XPATH, value="//tr/td[contains(., '" + coursesIdPatternMap[coursesId] + "')]"):
-    writeToFirestore('Not Enrolled',f"Could not find enrollement for {fln}", courseName=courseName, isQuit=True)
-else:
-    writeToFirestore('Enrolled',f"{id} successfully enrolled into {coursesId}", courseName=courseName)
+    # SET DUE DATES, CONFIRM SELECTION AND ENROLL STUDENT
+    expirationDate = date.today() + timedelta(days=const.ENROLL_PAGE_EXPIRATION_DAYS)
+    remindDate = expirationDate -  timedelta(days=const.ENROLL_PAGE_REMIND_BEFORE_EXP)
+    strExpirationDate = expirationDate.strftime(const.ENROLL_PAGE_DATE_FORMAT)
+    strRemindDate = remindDate.strftime(const.ENROLL_PAGE_DATE_FORMAT)
+    driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_REMINDER).clear()
+    driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_REMINDER).send_keys(strRemindDate)
+    driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_EXPIRATION).clear()
+    driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_EXPIRATION).send_keys(strExpirationDate)
+    driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_CONTINUE4).click()
+    driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_CONTINUE5).click()
+    if (args.enroll):
+        driver.find_element(by=By.ID, value=const.ENROLL_PAGE_ID_COMPLETE_ENROLL).click()
 
+    # VERIFY THAT STUDENT IS ACTUALLY ENROLLED
+    driver.get(const.VERIFY_PAGE_STUDENTS_PAGE)
+    txtId = driver.find_element(by=By.ID, value=const.STUDENT_PAGE_ID_ID)
+    txtId.clear()
+    txtId.send_keys(id)
+    try:
+        grid = driver.find_element(by=By.ID, value=const.STUDENT_PAGE_ID_GRID)
+        searchBtn = driver.find_element(by=By.ID, value=const.STUDENT_PAGE_ID_SEARCH)            
+        searchBtn.click()            
+        WebDriverWait(driver, 20).until(EC.staleness_of(grid))    
+    except:
+        writeToFirestore('Not Enrolled',f"Search: unable to submit user search for {fln}", courseName=courseName, isQuit=True)
+
+    if not is_element_present(by=By.ID, value=const.VERIFY_PAGE_ID_RECORD_ONE):
+        writeToFirestore('Not Enrolled',f"Verify enrollement: cannot find student with id {id} and name {fln}", courseName=courseName, isQuit=True)
+
+    # for each course, there must be identification. below is only for ELDT Theory
+    driver.find_element(by=By.ID, value=const.VERIFY_PAGE_ID_RECORD_ONE).click()
+    #if not is_element_present(by=By.ID, value=const.VERIFY_PAGE_ID_REC_37):
+    if not is_element_present(by=By.XPATH, value="//tr/td[contains(., '" + coursesIdPatternMap[coursesId] + "')]"):
+        writeToFirestore('Not Enrolled',f"Could not find enrollement for {fln}", courseName=courseName, isQuit=True)
+    else:
+        writeToFirestore('Enrolled',f"{id} successfully enrolled into {coursesId}", courseName=courseName)
+except Exception as e:
+    writeToFirestore('Not Enrolled',f"Exception occurred when registering {fln}: {e}", isQuit=True) 
 #time.sleep(30)
 #title = driver.title
 #print("Enroll was simulated")
