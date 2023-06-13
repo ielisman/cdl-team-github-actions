1ad7c6c3f2da8d3517a95d8c365d8a2239c995cd
ielisman <ielisman@gmail.com>
2023-06-13 17:25:53 -0400

    started with road test site booking automation

---
+++ road_test_constants.py
@@ -0,0 +1,18 @@
+DRIVER_PATH                     = r"C:/SeleniumDrivers/chrome"
+EXECUTABLE_PATH                 = r"C:/SeleniumDrivers/chrome/chromedriver.exe"
+ROAD_TEST_URL                   = r"https://www.nyakts.com/Login.aspx?ReturnUrl=%2fNyRstApps%2fThirdPartyBooking.aspx%3fmid%3d2269&mid=2269"
+IMPLICIT_WAIT                   = 15
+
+
+LOGIN_PAGE_ID_USERNAME          = "UserName"
+LOGIN_PAGE_ID_PASSWORD          = "Password"
+LOGIN_PAGE_ID_LOGIN             = "Login1_btnPreLoginButton"
+LOGIN_PAGE_ID_CONFIRM           = "confirmDialog" # in case someone is logged in manually
+
+DASH_URL                        = r"https://www.jjkellertraining.com/Views/Dashboard/MyDashboard.aspx"
+DASH_PAGE_ID_SIGNOUT            = "hlSignout"
+DASH_PAGE_VAL_SIGNOUT           = "Sign Out"
+DASH_PAGE_ATXT_MYSTUDENTS       = "My Students"
+DASH_PAGE_ID_ONLINETRAINING     = "hlInteractiveTraining"
+
+STUDENT_PAGE_ID_LASTNAME        = "ContentPlaceHolder1_txtLastName"
---
+++ test_rt.py
@@ -0,0 +1,32 @@
+import road_test_constants as const
+
+from selenium                           import webdriver
+from selenium.common.exceptions         import WebDriverException, NoSuchElementException
+from selenium.webdriver.chrome.service  import Service
+from selenium.webdriver.common.by       import By
+from selenium.webdriver.support         import expected_conditions as EC
+from selenium.webdriver.support.ui      import WebDriverWait, Select
+from webdriver_manager.chrome           import ChromeDriverManager
+
+def is_element_present(by, value):
+    try: driver.find_element(by=by, value=value)
+    except NoSuchElementException as e: return False
+    return True
+
+chrome_options = webdriver.ChromeOptions()
+#chrome_options.add_argument('--headless')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+try:
+    driver.get(const.ROAD_TEST_URL)
+    driver.find_element(by=By.ID, value=const.LOGIN_PAGE_ID_USERNAME).clear()
+    driver.find_element(by=By.ID, value=const.LOGIN_PAGE_ID_USERNAME).send_keys(os.environ['NYAKTS_USERNAME'])
+    driver.find_element(by=By.ID, value=const.LOGIN_PAGE_ID_PASSWORD).clear()
+    driver.find_element(by=By.ID, value=const.LOGIN_PAGE_ID_PASSWORD).send_keys(os.environ['NYAKTS_PASSWORD'])  
+    driver.find_element(by=By.ID, value=const.LOGIN_PAGE_ID_LOGIN).click()
+    if is_element_present(by=By.ID, value=const.LOGIN_PAGE_ID_CONFIRM):
+        driver.find_element(by=By.ID, value=const.LOGIN_PAGE_ID_CONFIRM).click()
+    
+
+except Exception as e:
+    print('Not Registered',f"Exception occurred: {e}")
